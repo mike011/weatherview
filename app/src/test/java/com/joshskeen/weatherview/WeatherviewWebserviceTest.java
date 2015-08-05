1dbc21b5863b60d726876405e317e5d29fda7750
mike011 <mike_ch_1@hotmail.com>
2015-08-05 14:32:51 -0400

    could not figure out injection

--- app/src/test/java/com/joshskeen/weatherview/WeatherviewWebserviceTest.java
+++ app/src/test/java/com/joshskeen/weatherview/WeatherviewWebserviceTest.java
@@ -14,36 +14,40 @@ import org.robolectric.annotation.Config;
 
 import java.util.List;
 
-import javax.inject.Inject;
-
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+import static junit.framework.Assert.assertNotNull;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 @RunWith(RobolectricTestRunner.class)
 @Config(emulateSdk = 18)
-public class WeatherviewWebserviceTest {
+public class WeatherviewWebserviceTest
+{
 
-    @Inject
     public WeatherServiceManager mWeatherServiceManager;
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(1111);
 
     @Before
-    public void setup() {
+    public void setup()
+    {
         Robolectric.getFakeHttpLayer().interceptHttpRequests(false);
+        String serviceEndpoint = "http://localhost:1111/api/" + BuildConfig.WEATHERVIEW_API_KEY + "/";
+        mWeatherServiceManager = new WeatherServiceManager(serviceEndpoint);
     }
 
     @Test
-    public void testGetCurrentWeatherReturnsExpected() {
+    public void testGetCurrentWeatherReturnsExpected()
+    {
         stubFor(get(urlMatching("/api/.*"))
                 .atPriority(5)
                 .willReturn(aResponse()
                         .withStatus(200)
                         .withBodyFile("atlanta-conditions.json")));
+        assertNotNull("Weather Service Manager is null", mWeatherServiceManager);
         List<WeatherCondition> conditionsForAtlanta = mWeatherServiceManager.getConditionsForAtlanta();
         assertThat(conditionsForAtlanta.size()).isEqualTo(1);
     }
